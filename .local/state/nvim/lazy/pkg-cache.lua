1695bd74bae9842c3e62ab593617a8d0f4b1388a
Anthony Nguyen <anthonybnguyen56@gmail.com>
2025-11-06 12:44:24 -0600

    add lazyvim default config

---
+++ .local/state/nvim/lazy/pkg-cache.lua
@@ -0,0 +1,10 @@
+return {pkgs={{spec=function()
+return {
+  -- nui.nvim can be lazy loaded
+  { "MunifTanjim/nui.nvim", lazy = true },
+  {
+    "folke/noice.nvim",
+  },
+}
+
+end,file="lazy.lua",dir="/Users/anbguye/.local/share/nvim/lazy/noice.nvim",source="lazy",name="noice.nvim",},{spec={"nvim-lua/plenary.nvim",lazy=true,},file="community",dir="/Users/anbguye/.local/share/nvim/lazy/plenary.nvim",source="lazy",name="plenary.nvim",},},version=12,}
