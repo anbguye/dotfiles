4f45643da934f74e2ba8507696487adb0b0dfc91
Anthony Nguyen <anthonybnguyen56@gmail.com>
2025-11-06 22:07:45 -0600

    add lazyvim plugins

--- .local/state/nvim/lazy/pkg-cache.lua
+++ .local/state/nvim/lazy/pkg-cache.lua
@@ -1,4 +1,4 @@
-return {pkgs={{spec=function()
+return {version=12,pkgs={{name="noice.nvim",dir="/Users/anbguye/.local/share/nvim/lazy/noice.nvim",spec=function()
 return {
   -- nui.nvim can be lazy loaded
   { "MunifTanjim/nui.nvim", lazy = true },
@@ -7,4 +7,4 @@ return {
   },
 }
 
-end,file="lazy.lua",dir="/Users/anbguye/.local/share/nvim/lazy/noice.nvim",source="lazy",name="noice.nvim",},{spec={"nvim-lua/plenary.nvim",lazy=true,},file="community",dir="/Users/anbguye/.local/share/nvim/lazy/plenary.nvim",source="lazy",name="plenary.nvim",},},version=12,}
+end,source="lazy",file="lazy.lua",},{name="plenary.nvim",dir="/Users/anbguye/.local/share/nvim/lazy/plenary.nvim",spec={"nvim-lua/plenary.nvim",lazy=true,},source="lazy",file="community",},},}
